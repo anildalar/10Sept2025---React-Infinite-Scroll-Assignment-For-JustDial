bbffda0510f0b9ffa45f17fc8aa91e17c8c60e93
anildalar <dollorinfotech@gmail.com>
2025-09-15 09:40:47 +0530

    new

--- frontend/src/pages/C.jsx
+++ frontend/src/pages/C.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import D from './D'
+import { UserContext } from './A';
 
 export default function C() {
+    const user = useContext(UserContext);
     return (
         <div>
             <h1>C Component</h1>
+            <h1>Hello, {user}</h1>
             <D />
         </div>
     )
